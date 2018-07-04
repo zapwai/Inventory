83aef2f9b9bd4c27d6bd6ce778090f0806a660f2
zapwai <zapwai@gmail.com>
2018-07-04 16:41:07 -0400

    updates

--- Inv.pm
+++ Inv.pm
@@ -6,18 +6,33 @@ require Exporter;
 @EXPORT = qw(insert_record create_db update_record remove_record load_db);
 
 sub load_db{
-my $driver = "SQLite";
-my $db_name = "inventory.db";
-my $dbd = "DBI:$driver:dbname=$db_name";
+    my $driver = "SQLite";
+    my $db_name = "inventory.db";
+    my $dbd = "DBI:$driver:dbname=$db_name";
 
-my $username = "";
-my $password = "";
+    my $username = "";
+    my $password = "";
 
-# The connection
-our $dbh = DBI->connect($dbd, $username, $password, { RaiseError => 1 })
-    or die $DBI::errstr;
+    # The connection
+    our $dbh = DBI->connect($dbd, $username, $password, { RaiseError => 1 })
+	or die $DBI::errstr;
 
-print STDERR "Database opened successfully.\n";
+    print STDERR "Database opened successfully.\n";
+}
+
+sub create_db{
+    my $stmt = qq(CREATE TABLE IF NOT EXISTS ITEMS
+(ID INTEGER PRIMARY KEY AUTOINCREMENT,
+TITLE CHAR(50) NOT NULL,
+QUANTITY INT NOT NULL,
+LOCATION CHAR(50)););
+
+    my $ret = $dbh -> do($stmt);
+    if ($ret < 0) {
+	print $DBI::errstr;
+    } else {
+	print "Table created successfully.\n";
+    }
 }
 
 sub close_db{
@@ -33,56 +48,76 @@ VALUES ('$title', $quantity, '$location'));
     $ret = $dbh->do($stmt) or die $DBI::errstr;
 }
 
-sub create_db{
-
-    my $stmt = qq(CREATE TABLE IF NOT EXISTS ITEMS
-(ID INTEGER PRIMARY KEY AUTOINCREMENT,
-TITLE CHAR(50) NOT NULL,
-QUANTITY INT NOT NULL,
-LOCATION CHAR(50)););
-
-    my $ret = $dbh -> do($stmt);
-    if ($ret < 0) {
-	print $DBI::errstr;
-    } else {
-	print "Table created successfully.\n";
-    }
-
-}
-
 sub update_record{
-    my $stmt = qq(UPDATE ITEMS set QUANTITY = 10 where TITLE='beer';);
+    my ($title, $quantity, $location) = @_;
+    my $stmt = qq(UPDATE ITEMS SET QUANTITY = $quantity, LOCATION = '$location' where TITLE='$title';);
     $ret = $dbh->do($stmt) or die $DBI::errstr;
     if ($ret < 0) {
 	print STDERR $DBI::errstr;
     }
-    
 }
 
 sub remove_record{
-    my $stmt = qq(DELETE from ITEMS where TITLE='chair';);
+    my $title = $_[0];
+    my $stmt = qq(DELETE from ITEMS where TITLE='$title';);
     $ret = $dbh->do($stmt) or die $DBI::errstr;
     if ($ret < 0) {
 	print STDERR $DBI::errstr;
     }
 }
 
-sub fetch_record{
-    my $stmt = qq(SELECT id, title, quantity, location from ITEMS;);
+sub filter_records{
+    my ($title, $location) = @_;
+    my $stmt;
+    if ($title){
+	$stmt = qq(SELECT id, title, quantity, location from ITEMS where TITLE = '$title';);
+    }
+    if ($title eq "") {
+	$stmt = qq(SELECT id, title, quantity, location from ITEMS where LOCATION GLOB '*$location*';);
+    }
+
     my $sth = $dbh -> prepare( $stmt );
     my $rv = $sth -> execute() or die $DBI::errstr;
-    if ($rv < 0){
+    if ($rv < 0) {
 	print $DBI::errstr;
     }
 
     while (my @row = $sth->fetchrow_array()) {
-	print "\nID = ".$row[0]."\n";
-	print "Item = ".$row[1]."\n";
-	print "Quantity = ".$row[2]."\n";
-	print "Location is ".$row[3]."\n";
-	print "There ";
-	if ($row[2] == 1) { print "is " }
-	else {print "are "}
+	#	print "\n\t\tID = ".$row[0]."\n";
+	print "\t\tItem: ".$row[1]."\n";
+	print "\t\tQuantity: ".$row[2]."\n";
+	print "\t\tLocation: ".$row[3]."\n";
+	print "\tThere ";
+	if ($row[2] == 1) {
+	    print "is ";
+	} else {
+	    print "are ";
+	}
+	print $row[2]." ".$row[1]."(s) in the ".$row[3].".\n";
+    }
+    
+}
+
+sub showall_records{
+    my $stmt = qq(SELECT id, title, quantity, location from ITEMS;);
+    my $sth = $dbh -> prepare( $stmt );
+    my $rv = $sth -> execute() or die $DBI::errstr;
+    if ($rv < 0) {
+	print $DBI::errstr;
+    }
+
+    while (my @row = $sth->fetchrow_array()) {
+	#	print "\n\t\tID = ".$row[0]."\n";
+	print "\t\tItem: ".$row[1]."\n";
+	print "\t\tQuantity: ".$row[2]."\n";
+	print "\t\tLocation: ".$row[3]."\n";
+	print "\tThere ";
+	if ($row[2] == 1) {
+	    print "is ";
+	} else {
+	    print "are ";
+	}
 	print $row[2]." ".$row[1]."(s) in the ".$row[3].".\n";
     }
 }
+
--- front.pl
+++ front.pl
@@ -8,7 +8,7 @@ use Inv;
 Inv::load_db();
 Inv::create_db();
 
-my $print_out = qq(\nPlease select an option: \n a\) add record\n d\) delete record \n e\) edit record\n s\) select records \n q\) quit\n);
+my $print_out = qq(\nPlease enter a key to select an option: \n \(a\)dd record, \(d\)elete record, \(e\)dit record, \(f\)ilter to see *some* records, \(s\)how all records, \(q\)uit\n);
 
 my $char="";
 while ($char ne "q"){
@@ -22,12 +22,32 @@ while ($char ne "q"){
 	chomp (my $location = <STDIN>);
 	Inv::insert_record($title, $quantity, $location);
     } elsif ($char eq "s"){
-	Inv::fetch_record();
-    } elsif ($char eq "d"){
-	Inv::remove_record();
-    } elsif ($char eq "e"){
-	Inv::update_record();
+	Inv::showall_records();
+    } elsif ($char eq "f"){
+	print "Leave blank to ignore a field.\n";
+	print "Name of item: ";
+ 	chomp (my $title = <STDIN>);
+	if (length $title == 0) {$title = ""};
+	print "Location of items: ";
+	chomp (my $location = <STDIN>);
+	if (length $location == 0) {$location = ""};
+	Inv::filter_records($title, $location);
     }
+
+    elsif ($char eq "d"){
+	print "Name of item to be removed: ";
+	chomp (my $title = <STDIN>);
+	Inv::remove_record($title);
+    } elsif ($char eq "e"){
+	print "Name of item to be updated: ";
+	chomp (my $title = <STDIN>);
+ 	print "Quantity of item: ";
+	chomp (my $quantity = <STDIN>);
+	$quantity = 0 if ( length $quantity == 0 );
+	print "Location of item: ";
+	chomp (my $location = <STDIN>);	
+	Inv::update_record($title, $quantity, $location);
+    } 
     
     print $print_out;
     chomp($char = <STDIN>);
